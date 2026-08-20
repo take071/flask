8bfe2525d2363ada9837c3b541e2617d3fb52399
Felipe <arthurfelipessousa@gmail.com>
2026-08-19 22:03:25 -0300

    Fix indentation and import statement in app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from app import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
- def home():
- return "hello word!":
+def home():
+   return "hello word!"
 
-  if __name__ == "__main__":
-      app.run(debug=True):
+if __name__ == "__main__":
+  app.run(debug=True)
