f8fabbe3a86bcc7155206c8515cc4fca520a0f51
Felipe <arthurfelipessousa@gmail.com>
2026-08-19 21:56:03 -0300

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,10 +1,10 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
- def home();
+ def home():
  return "hello word!":
 
-  if __name__ =="__main__":
- app.run(debug=True):
+  if __name__ == "__main__":
+      app.run(debug=True):
